49f3282ef442a895a0edfcdf13b00a3fcea36858
Joseph <Lkyjosephryan@gmail.com>
2025-06-30 21:17:15 -0400

    Learning the basics - 13/100 wins

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,33 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var captureMove = new Move();
+        for (int i = 0; i < moves.Length; i++)
+        {
+            board.MakeMove(moves[i]);
+            if (board.IsInCheck())
+            {
+                board.UndoMove(moves[i]);
+                return moves[i];
+            }
+            else board.UndoMove(moves[i]);
+        }
+        captureMove = moves.FirstOrDefault(x => x.CapturePieceType == PieceType.King, captureMove);
+        if (captureMove == new Move())
+        {
+            captureMove = moves.FirstOrDefault(x => x.IsCapture, new Move());
+            if (captureMove == new Move())
+            {
+                captureMove = moves.FirstOrDefault(x => x.MovePieceType == PieceType.Queen, moves[0]);
+            }
+        }
+        return captureMove;
     }
 }
