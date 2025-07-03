f64b49ba9b0ac9169cd28d6dbf5a1aec6a0628f4
Joseph <Lkyjosephryan@gmail.com>
2025-07-02 22:52:22 -0400

    Add Matecheck - 21/100

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
 using ChessChallenge.API;
@@ -7,7 +8,19 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        var color = board.IsWhiteToMove; // True is white, False is black   
         Move[] moves = board.GetLegalMoves();
+        for (int i = 0; i < moves.Length; i++)
+        {
+            board.MakeMove(moves[i]);
+            if (board.IsInCheckmate())
+            {
+                return moves[i];
+            }
+            board.UndoMove(moves[i]);
+        }
+
+
         var captureMove = new Move();
         for (int i = 0; i < moves.Length; i++)
         {
@@ -22,12 +35,38 @@ public class MyBot : IChessBot
         captureMove = moves.FirstOrDefault(x => x.CapturePieceType == PieceType.King, captureMove);
         if (captureMove == new Move())
         {
-            captureMove = moves.FirstOrDefault(x => x.IsCapture, new Move());
-            if (captureMove == new Move())
+            captureMove = moves.MaxBy(x => x.CapturePieceType);
+            if (captureMove.CapturePieceType <= 0)
             {
                 captureMove = moves.FirstOrDefault(x => x.MovePieceType == PieceType.Queen, moves[0]);
             }
         }
         return captureMove;
+    }
+    public Move[] MakeMoveAndGetLegalMoves(Move move, Board board)
+    {
+        board.MakeMove(move);
+        var LegalMoves = board.GetLegalMoves();
+        board.UndoMove(move);
+        return LegalMoves;
+    }
+
+    public decimal EvaluateBoard(Board board, bool color)
+    {
+        var pieceList = board.GetAllPieceLists();
+        var whiteScore = pieceList[0].Count
+        + (pieceList[1].Count * 3)
+        + (pieceList[2].Count * 3)
+        + (pieceList[3].Count * 5)
+        + (pieceList[4].Count * 9);
+
+        var blackScore = pieceList[6].Count
+        + (pieceList[7].Count * 3)
+        + (pieceList[8].Count * 3)
+        + (pieceList[9].Count * 5)
+        + (pieceList[10].Count * 9);
+
+        return color ? whiteScore / (whiteScore + blackScore) : blackScore / (whiteScore + blackScore);
+
     }
 }
